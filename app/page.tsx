fcbe34d7572bc644a0a316520522cc6c795a9223
laus1983 <luisurdaneta1983@gmail.com>
2023-08-09 21:05:27 -0400

    Agregando el estado al archivo page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
-import Image from "next/image";
+"use client"; //Directiva que configura el componente como cliente
+
+// import Image from "next/image";
+import { useState } from "react";
 import { RandomFox } from "../components/RandomFox";
 
+const getRandomInt = () => Math.floor(Math.random() * 123) + 1; //Generar un numero aleatorio entre 1 y 123
+
+const generateId = () => Math.random().toString(36).substr(2, 9); //generar un id unico para cada imagen
+
+type ImageInfo = { id: string; url: string };
+
 export default function Home() {
+  const [images, setImages] = useState<Array<ImageInfo>>([
+    {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
+    },
+    {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
+    },
+    {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
+    },
+    {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
+    },
+  ]);
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <RandomFox />
+      <h1 className="text-3xl font-bold underline">
+        Component Lazy Loading Image
+      </h1>
+      {images.map(({ id, url }) => (
+        <div className="p-4" key={id}>
+          <RandomFox image={url} />
+        </div>
+      ))}
     </main>
   );
 }
--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,7 +1,6 @@
-const getRandomInt = () => Math.floor(Math.random() * 123) + 1; //Generar un numero aleatorio entre 1 y 123
+type Props = { image: string };
 
-export const RandomFox = (): JSX.Element => {
-  const image = `https://randomfox.ca/images/${getRandomInt()}.jpg`; //Generar la url de la imagen
+export const RandomFox = ({ image }: Props): JSX.Element => {
   return (
     <div className="flex justify-center">
       <img src={image} alt="Random fox image" className="w-1/2" />
