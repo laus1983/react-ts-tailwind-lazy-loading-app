dbecec1234191feca9e1eba720c36a3a78b5c138
laus1983 <luisurdaneta1983@gmail.com>
2023-08-11 18:57:00 -0400

    Se agrego el boton para cargar mas imagenes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 // import Image from "next/image";
 import { useState } from "react";
+import type { MouseEventHandler } from "react";
 import { RandomFox } from "../components/RandomFox";
 
 const getRandomInt = () => Math.floor(Math.random() * 123) + 1; //Generar un numero aleatorio entre 1 y 123
@@ -11,29 +12,24 @@ const generateId = () => Math.random().toString(36).substr(2, 9); //generar un i
 type ImageInfo = { id: string; url: string };
 
 export default function Home() {
-  const [images, setImages] = useState<Array<ImageInfo>>([
-    {
+  const [images, setImages] = useState<Array<ImageInfo>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    const newImage: ImageInfo = {
       id: generateId(),
       url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
-    },
-    {
-      id: generateId(),
-      url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
-    },
-    {
-      id: generateId(),
-      url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
-    },
-    {
-      id: generateId(),
-      url: `https://randomfox.ca/images/${getRandomInt()}.jpg`,
-    },
-  ]);
+    };
+
+    setImages([...images, newImage]);
+  };
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
         Component Lazy Loading Image
       </h1>
+      <button onClick={addNewFox}>Cargar nueva imagen</button>
       {images.map(({ id, url }) => (
         <div className="p-4" key={id}>
           <RandomFox image={url} />
