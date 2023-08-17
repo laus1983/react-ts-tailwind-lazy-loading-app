e24e1af3f2a9ced710a95a241bc93d8ec56b82c7
laus1983 <luisurdaneta1983@gmail.com>
2023-08-16 20:27:58 -0400

    Se modifico el componente RandomFox con el lazy loading

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,9 +1,32 @@
+import { useRef, useEffect, useState } from "react";
 type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null);
+  const [isIntersecting, setIsIntersecting] = useState("");
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsIntersecting(image);
+        }
+      });
+    });
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => observer.disconnect();
+  }, [image]);
+
   return (
     <div className="flex justify-center">
-      <img src={image} alt="Random fox image" className="w-1/2" />
+      <img
+        ref={node}
+        src={isIntersecting}
+        alt="Random fox image"
+        className="w-1/2 rounded bg-gray-300"
+      />
     </div>
   );
 };
