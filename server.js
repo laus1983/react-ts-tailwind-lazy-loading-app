1473d4c751b3677603508e3305e66510d809ef7f
laus1983 <luisurdaneta1983@gmail.com>
2023-08-22 20:46:34 -0400

    Se realizo el build

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname =
-  process.env.NODE_ENV !== "production" ? "localhost" : "luisurdaneta.com";
-const port = process.env.PORT || 4789;
-
+const hostname = "localhost";
+const port = 3000;
+// when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true);
       const { pathname, query } = parsedUrl;
 
@@ -28,8 +29,12 @@ app.prepare().then(() => {
       res.statusCode = 500;
       res.end("internal server error");
     }
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
-  });
+  })
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
 });
